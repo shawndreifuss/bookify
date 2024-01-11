01aa036cd2b99ddf1b567144d98df1e5fc057793
Shawn Dreifuss <shawndreifuss@icloud.com>
2024-01-10 16:09:25 -0800

    troubleshoot server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv'); 
-const helmet = require('helmet');
-const morgan = require('morgan');
 const db = require('./config/connection');
 
 // Uncomment when routes are created
