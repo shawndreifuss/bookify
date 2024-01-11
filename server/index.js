778d76e9b1559814aff061c5c7d891894b0ccf43
Shawn Dreifuss <shawndreifuss@icloud.com>
2024-01-10 16:07:24 -0800

    addedd to index.js for server

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv'); 
+const helmet = require('helmet');
+const morgan = require('morgan');
+const db = require('./config/connection');
+
+// Uncomment when routes are created
+// const routes = require('./routes');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(routes)
+
+
+// comment when routes are created
+ app.get('/', async (req, res) => {
+   res.send('Hello World!')
+ })
+
+
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${PORT} running on port ${PORT}!`);
+  });
+});
